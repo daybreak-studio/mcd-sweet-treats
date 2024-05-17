5cf049af789310cc0eab0658c1c98104513fcf60
alvin leung <alvinleung2009@gmail.com>
2024-05-16 18:20:08 -0700

    add button animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,30 @@
+"use client";
+
 import React from "react";
 import TracyShadow from "../TracyShadow";
+import { motion } from "framer-motion";
 
 type Props = {
   children: string;
   inverted?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 };
 
-const Button = ({ children, inverted, onClick }: Props) => {
+const Button = ({ children, inverted, onClick, disabled }: Props) => {
   return (
     <TracyShadow color={"#643525"} elevation={1}>
-      <button
+      <motion.button
         onClick={onClick}
         className={`h-16 rounded-full font-serif-base min-w-64 ${
           inverted ? "bg-light text-dark" : "bg-dark text-light"
         }`}
+        whileTap={{
+          scale: 0.98,
+        }}
       >
         {children}
-      </button>
+      </motion.button>
     </TracyShadow>
   );
 };
