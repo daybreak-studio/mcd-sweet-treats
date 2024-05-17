6c8678888ef36689253c48cf6cdb9ac7f79ba995
alvin leung <alvinleung2009@gmail.com>
2024-05-16 17:33:57 -0700

    setup type system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import TypeSystemPlugin from "./TypeSystemPlugin";
 
 const config: Config = {
   content: [
@@ -15,6 +16,71 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // serifs
+    TypeSystemPlugin({
+      default: {
+        "serif-sm": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "14px",
+          "line-height": "20px",
+          "letter-spacing": "-0.02em",
+        },
+        "serif-base": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "16px",
+          "line-height": "24px",
+          "letter-spacing": "-0.02em",
+        },
+        "serif-md": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "20px",
+          "line-height": "28px",
+          "letter-spacing": "-0.02em",
+        },
+        "serif-lg": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "24px",
+          "line-height": "32px",
+          "letter-spacing": "-0.03em",
+        },
+        "serif-xl": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "32px",
+          "line-height": "36px",
+          "letter-spacing": "-0.01em",
+        },
+        "serif-2xl": {
+          "font-family": "bookman-jf-pro",
+          "font-size": "44px",
+          "line-height": "48px",
+          "letter-spacing": "-0.01em",
+        },
+      },
+    }),
+    // sans
+    TypeSystemPlugin({
+      default: {
+        "sans-xs": {
+          "font-family": "var(--font-speedee-regular)",
+          "font-size": "12px",
+          "letter-spacing": "0.02em",
+          "line-height": "20px",
+        },
+        "sans-sm": {
+          "font-family": "var(--font-speedee-regular)",
+          "font-size": "14px",
+          "letter-spacing": "0.01em",
+          "line-height": "24px",
+        },
+        "sans-base": {
+          "font-family": "var(--font-speedee-regular)",
+          "font-size": "16px",
+          "letter-spacing": "0.01em",
+          "line-height": "24px",
+        },
+      },
+    }),
+  ],
 };
 export default config;
