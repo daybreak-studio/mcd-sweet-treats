fa09d9781a0ad60f6b535b6a44e9d0f0efcac02c
alvin leung <alvinleung2009@gmail.com>
2024-05-22 10:02:39 -0700

    make app frame on top

--- src/components/AppFrame/AppFrame.tsx
+++ src/components/AppFrame/AppFrame.tsx
@@ -7,7 +7,7 @@ type Props = {
 const AppFrame = ({ children }: Props) => {
   return (
     <>
-      <div className="border-accent fixed inset-0 border-[12px] h-screen pointer-events-none"></div>
+      <div className="border-accent fixed inset-0 border-[12px] h-screen pointer-events-none z-20"></div>
       <div className="flex flex-col items-center justify-center bg-light text-dark min-h-screen">
         {children}
       </div>
