4958482d32c9a7ca2c7187d1e1f32338a2014671
alvin leung <alvinleung2009@gmail.com>
2024-05-24 12:49:29 -0700

    fix page routing problem

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,32 +1,18 @@
 "use client";
 
 import AppFrame from "@/components/AppFrame/AppFrame";
-import { useUserInfo } from "@/components/UserInfoProvider/UserInfoProvider";
 import VideoRecorder from "@/components/VideoRecorder/VideoRecorder";
-import {
-  InputLanguageKey,
-  OutputLanguageKey,
-} from "@/components/VideoUploadProvider/Languages";
-import { useVideoUpload } from "@/components/VideoUploadProvider/VideoUploadProvider";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
 const RecordPage = (props: Props) => {
-  const { upload } = useVideoUpload();
-  const { email, name, inputLanguage, outputLanguage } = useUserInfo();
   const router = useRouter();
 
   const handleRecordingComplete = async (blob: Blob) => {
-    // initiate the upload
-    upload(blob, {
-      name: name,
-      email: email,
-      inputLanguage: inputLanguage as InputLanguageKey,
-      outputLanguage: outputLanguage as OutputLanguageKey,
-    });
-    router.push("/uploading");
+    // let the user fill in the info
+    router.push("/user-info");
   };
 
   return (
