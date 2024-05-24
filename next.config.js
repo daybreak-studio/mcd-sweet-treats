96c2d704cd39e16f2ae740abe3eacfbfe943f7c8
alvin leung <alvinleung2009@gmail.com>
2024-05-24 11:50:36 -0700

    add record button

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withSvgr = require("next-plugin-svgr");
+
 module.exports = {
   reactStrictMode: false,
 };
+
+module.exports = withSvgr({
+  reactStrictMode: false,
+  // webpack(config, options) {
+  //   return config;
+  // },
+});
