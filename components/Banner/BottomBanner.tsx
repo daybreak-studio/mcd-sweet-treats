b17ffed81f06991e60d164e86fde2f2110e19793
alvin leung <alvinleung2009@gmail.com>
2024-05-28 12:55:35 -0700

    add minimum margin top for bottom banner

--- components/Banner/BottomBanner.tsx
+++ components/Banner/BottomBanner.tsx
@@ -1,18 +1,18 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-const BottomBanner = ({
-  children
-}: {
-  children: React.ReactNode
-}) => {
-  return <>
-    <motion.div className="font-sans-xs mb-8 opacity-50 text-center mx-auto"
-      transition={{ delay: 1.5 }} initial={{ opacity: 0, y: 16, }} animate={{ opacity: 1, y: 0 }}
-    >
-      {children}
-
-    </motion.div>
-  </>
-}
+const BottomBanner = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <motion.div
+        className="font-sans-xs mx-auto mb-0 mt-auto pt-6 text-center opacity-50"
+        transition={{ delay: 1.5 }}
+        initial={{ opacity: 0, y: 16 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
 
 export default BottomBanner;
