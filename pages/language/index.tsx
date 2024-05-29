22b79ff9bd560412ac186e5fd377b6d7aff67bfc
alvin leung <alvinleung2009@gmail.com>
2024-05-28 20:11:35 -0700

    language page

--- pages/language/index.tsx
+++ pages/language/index.tsx
@@ -12,6 +12,7 @@ import {
 import { motion } from "framer-motion";
 import { AnimWrap } from "@/components/AnimWrap";
 import BottomBanner from "@/components/Banner/BottomBanner";
+import ImageCollage from "@/components/ImageCollage/ImageCollage";
 const supportedInputLanguages = Object.keys(inputLanguageMap);
 const supportedOutputLanguages = Object.keys(outputLanguageMap);
 
@@ -20,49 +21,54 @@ export default function LanguagePage() {
     useUserInfo();
 
   return (
-    <AppFrame>
-      <LogoLockup />
-      <motion.div
-        variants={AnimWrap.AnimParentA}
-        initial="hidden"
-        animate="visible"
-        className="my-8 flex flex-grow flex-col items-center justify-center"
-      >
-        <motion.h1
-          variants={AnimWrap.bounceUpA}
-          className="font-serif-xl mb-4 w-8/12 text-center"
+    <>
+      <AppFrame>
+        <LogoLockup />
+        <motion.div
+          variants={AnimWrap.AnimParentA}
+          initial="hidden"
+          animate="visible"
+          className="my-8 flex flex-grow flex-col items-center justify-center"
         >
-          Select your language
-        </motion.h1>
-        <div className="mb-12 flex w-64 flex-col gap-4">
-          <Dropdown.menu
-            label={"From"}
-            onChange={(latest) => setInputLanguage(latest as InputLanguageKey)}
-            value={inputLanguage}
+          <motion.h1
+            variants={AnimWrap.bounceUpA}
+            className="font-serif-xl mb-4 w-8/12 text-center"
           >
-            {supportedInputLanguages.map((language, index) => (
-              <Dropdown.item value={language} key={index}>
-                {language}
-              </Dropdown.item>
-            ))}
-          </Dropdown.menu>
-          <Dropdown.menu
-            label={"To"}
-            onChange={(latest) =>
-              setOutputLanguage(latest as OutputLanguageKey)
-            }
-            value={outputLanguage}
-          >
-            {supportedOutputLanguages.map((language, index) => (
-              <Dropdown.item value={language} key={index}>
-                {language}
-              </Dropdown.item>
-            ))}
-          </Dropdown.menu>
-        </div>
-        <LinkButton href={"/record"}>{"Let's go"}</LinkButton>
-      </motion.div>
-      <BottomBanner>{"Select Languages Available"}</BottomBanner>
-    </AppFrame>
+            Select your language
+          </motion.h1>
+          <div className="mb-12 flex w-64 flex-col gap-4">
+            <Dropdown.menu
+              label={"From"}
+              onChange={(latest) =>
+                setInputLanguage(latest as InputLanguageKey)
+              }
+              value={inputLanguage}
+            >
+              {supportedInputLanguages.map((language, index) => (
+                <Dropdown.item value={language} key={index}>
+                  {language}
+                </Dropdown.item>
+              ))}
+            </Dropdown.menu>
+            <Dropdown.menu
+              label={"To"}
+              onChange={(latest) =>
+                setOutputLanguage(latest as OutputLanguageKey)
+              }
+              value={outputLanguage}
+            >
+              {supportedOutputLanguages.map((language, index) => (
+                <Dropdown.item value={language} key={index}>
+                  {language}
+                </Dropdown.item>
+              ))}
+            </Dropdown.menu>
+          </div>
+          <LinkButton href={"/record"}>{"Let's go"}</LinkButton>
+        </motion.div>
+        <BottomBanner>{"Select Languages Available"}</BottomBanner>
+      </AppFrame>
+      <ImageCollage />
+    </>
   );
 }
