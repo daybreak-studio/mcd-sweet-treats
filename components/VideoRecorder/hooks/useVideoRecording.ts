3f3069107066d836d45bcbe2ca838d4a77b8d7fe
Christopher Ha <contact@bhris.digital>
2024-05-31 10:07:28 -0400

    Add new fix for aspect ratio handling
    Using user agent now instead of purely browser size.

--- components/VideoRecorder/hooks/useVideoRecording.ts
+++ components/VideoRecorder/hooks/useVideoRecording.ts
@@ -12,8 +12,48 @@ export function useVideoRecording(
   const [isMediaRecorderReady, setIsMediaRecorderReady] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
 
-  const { isMobile } = useViewport();
-  const aspectRatio = useMemo(() => (isMobile ? 16 / 9 : 9 / 16), [isMobile]);
+  // Deprecate usage of isMoble for aspect ratio handling
+  // const { isMobile } = useViewport();
+
+  // Get device type
+  const getDeviceType = () => {
+    const userAgent =
+      // Make sure window is not undefined and navigator exists before accessing userAgent
+      typeof window !== "undefined" && window.navigator
+        ? window.navigator.userAgent
+        : "";
+    const mobile = Boolean(
+      userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i,
+      ),
+    );
+    const tablet = Boolean(userAgent.match(/Tablet|iPad/i));
+    if (tablet) {
+      return "tablet";
+    } else if (mobile) {
+      return "mobile";
+    } else {
+      return "desktop";
+    }
+  };
+
+  // Get device type on load
+  const deviceType = useMemo(getDeviceType, []);
+
+  // Get aspect ratio based on device type
+  const aspectRatio = useMemo(() => {
+    switch (deviceType) {
+      case "mobile":
+        return 16 / 9;
+      case "tablet":
+        return 9 / 16;
+      case "desktop":
+        return 9 / 16;
+      default:
+        // If we somehow do not match any of these, default to 9/16 as safety net.
+        return 9 / 16;
+    }
+  }, [deviceType]);
 
   useEffect(() => {
     if (!(canvasElm instanceof HTMLCanvasElement) || !videoElm) return;
