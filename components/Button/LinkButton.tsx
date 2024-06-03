6ef4713f7731b6943b1c35b6fea095066b4b0a4d
Christopher Ha <contact@bhris.digital>
2024-06-03 11:50:15 -0400

    Temp disable TracyShadow
    Causing animate out bug due to Safari blur.

--- components/Button/LinkButton.tsx
+++ components/Button/LinkButton.tsx
@@ -14,54 +14,63 @@ type Props = {
 
 const LinkButtonVariants = {
   hidden: {
-    rotateX: -50, rotateY: 0, rotateZ: 20,
-    scale: .8, opacity: 0,
+    rotateX: -50,
+    rotateY: 0,
+    rotateZ: 20,
+    scale: 0.8,
+    opacity: 0,
     y: 20,
-
   },
   visible: {
-    rotateX: 0, rotateY: 0, rotateZ: 0, scale: 1, opacity: [0, 1, 1], y: 0,
+    rotateX: 0,
+    rotateY: 0,
+    rotateZ: 0,
+    scale: 1,
+    opacity: [0, 1, 1],
+    y: 0,
 
     transition: {
       type: "spring",
-      bounce: .4,
-      duration: .5,
-    }
-  }
-}
+      bounce: 0.4,
+      duration: 0.5,
+    },
+  },
+};
 const LinkButton = ({ inverted, href, children, disabled }: Props) => {
   return (
-    <TracyShadow variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
-      color={"#643525"} elevation={1}>
-      <motion.div
-        className={`rounded-full outline-accent overflow-hidden ${inverted ? "bg-dark" : "bg-light"
-          }`}
-        whileTap={{
-          scale: !disabled ? 0.98 : 1,
+    // <TracyShadow variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+    // color={"#643525"} elevation={1}>
+    <motion.div
+      className={`overflow-hidden rounded-full outline-accent ${
+        inverted ? "bg-dark" : "bg-light"
+      }`}
+      whileTap={{
+        scale: !disabled ? 0.98 : 1,
+      }}
+      variants={LinkButtonVariants}
+    >
+      <Link
+        href={href}
+        onClick={(e) => {
+          if (disabled) e.preventDefault();
         }}
-        variants={LinkButtonVariants}
+        style={{
+          cursor: disabled ? "default" : "pointer",
+        }}
       >
-        <Link
-          href={href}
-          onClick={(e) => {
-            if (disabled) e.preventDefault();
-          }}
-          style={{
-            cursor: disabled ? "default" : "pointer",
+        <motion.div
+          className={`pointer-events-none h-16 ${
+            inverted ? "bg-light text-dark" : "bg-dark text-light"
+          } font-serif-base flex min-w-64 items-center justify-center rounded-full text-center`}
+          animate={{
+            opacity: disabled ? 0.2 : 1,
           }}
         >
-          <motion.div
-            className={`h-16 pointer-events-none ${inverted ? "bg-light text-dark" : "bg-dark text-light"
-              } flex items-center text-center justify-center rounded-full font-serif-base min-w-64`}
-            animate={{
-              opacity: disabled ? 0.2 : 1,
-            }}
-          >
-            {children}
-          </motion.div>
-        </Link>
-      </motion.div>
-    </TracyShadow >
+          {children}
+        </motion.div>
+      </Link>
+    </motion.div>
+    // </TracyShadow >
   );
 };
 
